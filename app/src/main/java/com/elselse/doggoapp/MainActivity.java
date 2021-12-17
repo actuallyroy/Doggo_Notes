071033463498d7897dabe2c26d7eb58e7781a576
Roy <roy93525@gmail.com>
2021-12-18 03:05:07 +0530

    fourth

--- app/src/main/java/com/elselse/doggoapp/MainActivity.java
+++ app/src/main/java/com/elselse/doggoapp/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends Activity {
         SharedPreferences.Editor prefsEditor = prefs.edit();
 
         TextView welcomeTxt = findViewById(R.id.wlcmTxt);
-        welcomeTxt.setText("Welcome, "+ prefs.getString(prefs.getString("Logged", "")+"name",""));
+        welcomeTxt.setText(prefs.getString(prefs.getString("Logged", "")+"name",""));
         ConstraintLayout constraintLayout = findViewById(R.id.cL);
         ImageButton logoutButton = findViewById(R.id.logoutBtn);
         logoutButton.setOnClickListener(new View.OnClickListener() {
