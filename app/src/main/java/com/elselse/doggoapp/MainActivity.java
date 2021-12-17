d9d9590ca577344863c234d509de04dcb2f90d9a
Roy <roy93525@gmail.com>
2021-12-18 03:04:29 +0530

    fourth

--- app/src/main/java/com/elselse/doggoapp/MainActivity.java
+++ app/src/main/java/com/elselse/doggoapp/MainActivity.java
@@ -1,21 +1,18 @@
 package com.elselse.doggoapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.ConstraintSet;
-
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         TextView welcomeTxt = findViewById(R.id.wlcmTxt);
         welcomeTxt.setText("Welcome, "+ prefs.getString(prefs.getString("Logged", "")+"name",""));
         ConstraintLayout constraintLayout = findViewById(R.id.cL);
-        Button logoutButton = findViewById(R.id.logoutBtn);
+        ImageButton logoutButton = findViewById(R.id.logoutBtn);
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
