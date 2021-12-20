eee64810037e95fe1311b273041f62c5223b032e
Roy <roy93525@gmail.com>
2021-12-20 18:14:28 +0530

    19/12/21 07:03 PM

--- app/src/main/java/com/elselse/doggoapp/MainActivity.java
+++ app/src/main/java/com/elselse/doggoapp/MainActivity.java
@@ -6,29 +6,44 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.InputType;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.animation.Animation;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Objects;
 
 public class MainActivity extends Activity {
 
     int Red, Green, Blue;
     LinearLayout profileMenu;
+    String userName;
 
-
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onAttachedToWindow() {
         super.onAttachedToWindow();
@@ -40,21 +55,64 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        refreshActivity();
         SharedPreferences prefs = getSharedPreferences("Accounts", MODE_PRIVATE);
         SharedPreferences.Editor prefsEditor = prefs.edit();
 
         profileMenu = findViewById(R.id.profile_menu);
         TextView welcomeTxt = findViewById(R.id.wlcmTxt),
                 userNameTxt = findViewById(R.id.userNameTxt),
-                aboutBtn = findViewById(R.id.aboutBtn);
+                aboutBtn = findViewById(R.id.aboutBtn),
+                textRepeat = findViewById(R.id.textRepeat),
+                textDue = findViewById(R.id.textDue),
+                title = findViewById(R.id.title);
+
         LinearLayout addNote = findViewById(R.id.addNote);
         FloatingActionButton fab = findViewById(R.id.fab);
         Button doneBtn = findViewById(R.id.doneBtn),
                 cancelBtn = findViewById(R.id.cancel_button);
 
+        EditText reminderDate = findViewById(R.id.reminderDate),
+                dueDate = findViewById(R.id.dueDate),
+                titleText = findViewById(R.id.titleText),
+                descriptionText = findViewById(R.id.descriptionText);
 
-        String userName = prefs.getString("Logged", "");
+        RadioGroup getRemRadioGrp = findViewById(R.id.getRemRadioGrp),
+                repeatRadioGrp = findViewById(R.id.repeatRadioGrp),
+                dueRadioGrp = findViewById(R.id.dueRadioGrp);
+
+        getRemRadioGrp.check(R.id.remRadioBtnOne);
+        repeatRadioGrp.check(R.id.repeatRadioGrpNoRepeat);
+        dueRadioGrp.check(R.id.dueRadioGrpNo);
+
+        getRemRadioGrp.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if(i == R.id.remRadioBtnOne){
+                    title.setText("Add Note");
+                    descriptionText.setVisibility(View.VISIBLE);
+                    reminderDate.setVisibility(View.GONE);
+                    textRepeat.setVisibility(View.GONE);
+                    repeatRadioGrp.setVisibility(View.GONE);
+                    textDue.setVisibility(View.GONE);
+                    dueRadioGrp.setVisibility(View.GONE);
+                    dueDate.setVisibility(View.GONE);
+                }else{
+                    title.setText("Add Task");
+                    descriptionText.setVisibility(View.GONE);
+                    reminderDate.setVisibility(View.VISIBLE);
+                    textRepeat.setVisibility(View.VISIBLE);
+                    repeatRadioGrp.setVisibility(View.VISIBLE);
+                    textDue.setVisibility(View.VISIBLE);
+                    dueRadioGrp.setVisibility(View.VISIBLE);
+                    dueDate.setVisibility(View.VISIBLE);
+                    if(i == R.id.remRadioBtnTwo){
+
+                    }
+                }
+            }
+        });
+        userName = prefs.getString("Logged", "");
         String name = prefs.getString(userName+"name","");
         welcomeTxt.setText(name);
         userNameTxt.setText(name.substring(0,1));
@@ -79,7 +137,12 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View view) {
                 addNote.setVisibility(View.VISIBLE);
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss aa");
+                Date now = new Date();
+                reminderDate.setText(sdf.format(now));
+                dueDate.setText(sdf.format(now));
             }
+
         });
 
         doneBtn.setOnClickListener(new View.OnClickListener() {
@@ -92,8 +155,17 @@ public class MainActivity extends Activity {
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
+                        if(getRemRadioGrp.getCheckedRadioButtonId() == R.id.remRadioBtnOne) {
+                            SharedPreferences prefs = getSharedPreferences(userName+"Notes", MODE_PRIVATE);
+                            SharedPreferences.Editor prefsEditor = prefs.edit();
+                            prefsEditor.putString(titleText.getText().toString(), descriptionText.getText().toString());
+                            prefsEditor.apply();
+                            titleText.setText("");
+                            descriptionText.setText("");
+                            getRemRadioGrp.check(R.id.remRadioBtnOne);
+                        }
                         drawable.clearColorFilter();
-                        //addNote.setVisibility(View.INVISIBLE);
+                        addNote.setVisibility(View.INVISIBLE);
                     }
                 },200);
             }
@@ -111,6 +183,16 @@ public class MainActivity extends Activity {
                     public void run() {
                         drawable.clearColorFilter();
                         addNote.setVisibility(View.INVISIBLE);
+                        title.setText("Add Note");
+                        descriptionText.setVisibility(View.VISIBLE);
+                        reminderDate.setVisibility(View.GONE);
+                        textRepeat.setVisibility(View.GONE);
+                        repeatRadioGrp.setVisibility(View.GONE);
+                        textDue.setVisibility(View.GONE);
+                        dueRadioGrp.setVisibility(View.GONE);
+                        dueDate.setVisibility(View.GONE);
+                        titleText.setText("");
+                        descriptionText.setText("");
                     }
                 },200);
             }
@@ -146,7 +228,6 @@ public class MainActivity extends Activity {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
 
-                Toast.makeText(getApplicationContext(), "Don't touch me!!", Toast.LENGTH_SHORT).show();
                 hideProfileMenu();
 
                 addNote.setVisibility(View.INVISIBLE);
@@ -188,5 +269,11 @@ public class MainActivity extends Activity {
                 profileMenu.setVisibility(View.INVISIBLE);
             }
         }, 300);
+    }
+
+    public void refreshActivity(){
+        SharedPreferences preferences = getSharedPreferences(userName+"Notes", MODE_PRIVATE);
+        int i = preferences.getAll().size();
+        Log.d("size", String.valueOf(i));
     }
 }
